da6c8d6c59394da52b1a6b00d9e40cb3b217bdd0
İsa Akay <akayisa955@gmail.com>
2023-08-30 19:29:22 +0300

    bazı degisiklikler2

--- js/custom/extra.js
+++ js/custom/extra.js
@@ -5,13 +5,15 @@ window.addEventListener("DOMContentLoaded", event => {
         // Sayfa üzerinde "mainNavbar" id'sine sahip bir öğeyi bul
         const nCollapsible = document.body.querySelector("#mainNavbar");
 
+        const myHeaderHeight = document.querySelector("#myHeader").offsetHeight;
+
         // Eğer böyle bir öğe yoksa fonksiyondan çık
         if (!nCollapsible) {
             return;
         }
 
         // Eğer pencerenin dikey kaydırma pozisyonu (screenY) 0 ise (en üstteyken)
-        if (window.scrollY === 0) {
+        if (window.scrollY < myHeaderHeight) {
             // "navbar-mobile" sınıfını "mainNavbar" öğesinden kaldır
             nCollapsible.classList.remove("navbar-mobile");
         } else {
@@ -29,9 +31,60 @@ window.addEventListener("DOMContentLoaded", event => {
 });
 
 
-var BtnCanvas=document.querySelectorAll(".btn-close-canvas");
-for(let i=0; i<BtnCanvas.length;i++){
-    BtnCanvas[i].addEventListener("click",function(){
-document.querySelector('[data-bs-dismiss="offcanvas"]').click();
+// Canvas kapatma düğmeleri için etkileşim kodu
+var BtnCanvas = document.querySelectorAll(".btn-close-canvas");
+for (let i = 0; i < BtnCanvas.length; i++) {
+    BtnCanvas[i].addEventListener("click", function() {
+        document.querySelector('[data-bs-dismiss="offcanvas"]').click();
     });
 }
+
+(function() {
+    'use strict';
+
+    var myForm = document.querySelector("#frmContact");
+    var myBtn = document.querySelector("#btnContact");
+    var currentCharacterCount = document.getElementById("current-character");
+
+    function updateCharacterCount() {
+        currentCharacterCount.textContent = myMessage.value.length;
+    }
+
+    function updateButtonState() {
+        if (myMessage.value.length >= 10) {
+            myBtn.disabled = false;
+        } else {
+            myBtn.disabled = true;
+        }
+    }
+
+    var myMessage = document.querySelector("#message");
+    myMessage.addEventListener("input", function() {
+        updateCharacterCount();
+        updateButtonState();
+    });
+
+    myForm.addEventListener("submit", function(event) {
+        event.preventDefault(); // Sayfanın yeniden yüklenmesini engelle
+
+        var formData = new FormData(myForm);
+
+        // Form verilerini sunucuya göndermek için uygun yöntemi kullanın,
+        // örneğin AJAX veya Fetch API
+
+        // Simüle edilmiş bir AJAX isteği
+        setTimeout(function() {
+            alert("Mesajınız başarıyla gönderildi!");
+
+            // Formu sıfırla
+            myForm.reset();
+            updateCharacterCount();
+            updateButtonState();
+        }, 1000); // 1 saniye bekletelim
+
+    });
+
+    // Sayfa yüklendiğinde de kontrolü yap
+    updateCharacterCount();
+    updateButtonState();
+})();
